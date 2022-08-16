a3efaa573aebc9717e78af12e75aa2cb3f77642c
Mat13 <matteo.lungo.13@gmail.com>
2022-08-16 11:12:51 +0200

    Completed manga search functionality

--- bot.js
+++ bot.js
@@ -30,31 +30,54 @@ async function main(context) {
         var start_date = formatDate(result.start_date);
         var end_date = formatDate(result.end_date);
         if (result.start_season) {
-            var start_season = result.start_season.season.replace('fall', 'autunno').replace('spring', 'primavera').replace('summer', 'estate').replace('winter', 'inverno') + ' ' + result.start_season.year;
+            var start_season = result.start_season.season.replace('fall', 'Autunno').replace('spring', 'Primavera').replace('summer', 'Estate').replace('winter', 'Inverno') + ' ' + result.start_season.year;
         } else {
             var start_season = "-"
         }
         var grade = result.mean !== undefined ? result.mean : "-";
         var media_type = await translate(result.media_type, { to: 'it' });
-        var status = await translate(result.status.replace('finished_airing', 'Concluso').replace('currently_airing', 'In Corso').replace('not_yet_aired', 'Non ancora trasmesso'), { to: 'it' });
+        var status = result.status.replace('finished_airing', 'Concluso').replace('currently_airing', 'In Corso').replace('not_yet_aired', 'Non ancora trasmesso');
         var genres = await translate(result.genres.map(i => i.name).join(', '), { to: 'it' });
         var num_episodes = result.num_episodes;
         var avg_episode_duration = Math.floor(result.average_episode_duration / 60) !== 0 ? Math.floor(result.average_episode_duration / 60) : null;
         var episodes = avg_episode_duration ? num_episodes + ' (~' + avg_episode_duration + ' min.)' : num_episodes;
         var studios = result.studios.map(i => i.name).join(', ');
+        var mal_link = 'https://myanimelist.net/anime/' + result.id;
         if (result.synopsis)
             var synopsis = await translate(result.synopsis.replace('[Written by MAL Rewrite]', ''), { to: 'it' });
-        var desc = '<b>Titolo: </b>' + title + ' (' + original_title + ')\n<b>Data Uscita: </b>' + start_season.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) +
-            '\n<b>Voto: </b>' + grade + '\n<b>Tipo: </b>' + media_type.charAt(0).toUpperCase() + media_type.slice(1) + '\n<b>Stato: </b>' + status.replace('_', ' ').charAt(0).toUpperCase() + status.slice(1) +
+        var desc = '<b>Titolo: </b>' + title + ' (' + original_title + ')\n<b>Data Uscita: </b>' + start_season +
+            '\n<b>Voto: </b>' + grade + '\n<b>Tipo: </b>' + media_type.charAt(0).toUpperCase() + media_type.slice(1) + '\n<b>Stato: </b>' + status +
             '\n<b>Genere: </b>' + genres.replace(/(^\w|\s\w)/g, m => m.toUpperCase()).replace('Cast Per Adulti', 'Cast Adulto') +
-            '\n<b>Episodi: </b>' + episodes + '\n<b>Studio: </b>' + studios;
+            '\n<b>Episodi: </b>' + episodes + '\n<b>Studio: </b>' + studios + '\n\n<b><a href="' + mal_link + '">Link MAL</a></b>';
         context.replyWithPhoto({ url: pic_url }, { caption: desc, parse_mode: 'HTML' }).then(() => {
-            if(synopsis) {
+            if (synopsis) {
                 context.reply(synopsis.substring(0, 500) + '[...]');
             }
         });
     } else if (flag === 'manga') {
         result = await utils.getManga(search);
+        var pic_url = result.main_picture.large;
+        var title = result.title;
+        var original_title = result.alternative_titles.ja;
+        var start_date = formatDate(result.start_date);
+        var grade = result.mean !== undefined ? result.mean : "-";
+        var media_type = result.media_type;
+        var status = result.status.replace('finished', 'Concluso').replace('currently_publishing', 'In Corso').replace('not_yet_published', 'Non ancora pubblicato').replace('on_hiatus', 'In pausa').replace('discontinued', 'Interrotto');
+        var genres = await translate(result.genres.map(i => i.name).join(', '), { to: 'it' });
+        var authors = result.authors.map(i => i.node.first_name + ' ' + i.node.last_name).join(', ');
+        var num_volumes = result.num_volumes !== 0 ? result.num_volumes : "-"
+        var mal_link = 'https://myanimelist.net/manga/' + result.id;
+        if (result.synopsis)
+            var synopsis = await translate(result.synopsis.replace('[Written by MAL Rewrite]', ''), { to: 'it' });
+        var desc = '<b>Titolo: </b>' + title + ' (' + original_title + ')\n<b>Data Uscita: </b>' + start_date +
+            '\n<b>Voto: </b>' + grade + '\n<b>Tipo: </b>' + media_type.charAt(0).toUpperCase() + media_type.slice(1) + '\n<b>Stato: </b>' + status +
+            '\n<b>Genere: </b>' + genres.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) +
+            '\n<b>Volumi: </b>' + num_volumes + '\n<b>Autori: </b>' + authors + '\n\n<b><a href="' + mal_link + '">Link MAL</a></b>';
+        context.replyWithPhoto({ url: pic_url }, { caption: desc, parse_mode: 'HTML' }).then(() => {
+            if (synopsis) {
+                context.reply(synopsis.substring(0, 500) + '[...]');
+            }
+        });
     } else {
         context.reply('Nessun risultato trovato.');
     }
